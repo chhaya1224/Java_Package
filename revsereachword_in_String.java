ac502dc193aa43d608effc24000bce4c250a18f9
chhaya1224 <73585030+chhaya1224@users.noreply.github.com>
2026-03-05 12:23:21 +0530

    Create revsereachword_in_String.java

---
+++ revsereachword_in_String.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {        
+        String str = "java selenium";
+        String words[] = str.split(" ");
+
+        for(String word : words){
+            String rev = "";
+
+            for(int i = word.length() - 1; i >= 0; i--){
+                rev = rev + word.charAt(i);
+            }
+
+            System.out.print(rev + " ");
+        }
+    }
+}
+===========================
+  output: avaj muineles
